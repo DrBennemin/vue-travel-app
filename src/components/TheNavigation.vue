1f52b5e9783273ad74ad57eb04dd349616c549da
Benjamin Krzis <benjamin@elisana.digital>
2021-03-15 14:35:32 +0100

    dynamic routing

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,28 +1,59 @@
 <template>
   <nav id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/brazil">Brazil</router-link>
-      <router-link to="/panama">Panama</router-link>
-      <router-link to="/hawaii">Hawaii</router-link>
-      <router-link to="/jamaica">Jamaica</router-link>
+      <ul class="nav-links">
+        <li class="links">
+          <router-link to="/">
+            Home
+          </router-link>
+        </li>
+        <li v-for="destination in destinations" :key="destination.name" class="links">
+          <router-link :to="{
+            name: 'DestinationDetails',
+            params: {id:destination.id}
+            }">
+            {{destination.name}}
+          </router-link>
+        </li>
+      </ul>
   </nav>
 </template>
+<script>
+import store from '@/store.js';
+export default {
+  data(){
+    return {
+      destinationsId: this.$route.params.id,
+      destinations: store.destinations
+    }
+  }
+}
+</script>
 
 <style scoped>
 #nav {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 30px;
+  display: flex;
 }
-
 #nav a {
-  font-weight: bold;
   color: #2c3e50;
-  padding: 0 10px;
+  text-decoration: none;
+  font-weight: bold;
 }
-
-#nav a.router-link-exact-active {
-  color: darkolivegreen;
+#nav a.vue-school-active-class {
+  color: #ab26ab;
+}
+.nav-links {
+  display: flex;
+}
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+.links:hover {
+  text-decoration: underline;
+}
+.logo {
+  font-size: 20px;
+  color: purple;
+  font-weight: bold;
 }
 </style>
